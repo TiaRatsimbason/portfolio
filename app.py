69647bf216d9b268d56142f4f69cbae066f03a3b
TiaRatsimbason <ratsimbason.tia@gmail.com>
2025-04-06 15:32:38 +0200

    modification apparence mobile

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
-from flask import Flask
-import os
-import logging
-from routes_package.routes import configure_routes # Importer les routes
+from flask import Flask 
+import os 
+import logging 
+from routes_package.routes import configure_routes # Importer les routes  
 
-app = Flask(__name__)
-logging.basicConfig(level=logging.DEBUG)  # Ajout du logging
+app = Flask(__name__) 
+logging.basicConfig(level=logging.DEBUG)  # Ajout du logging  
 
-# Configuration pour les fichiers statiques
-app.static_folder = 'static'
+# Configuration pour les fichiers statiques 
+app.static_folder = 'static'  
 
-# Assurez-vous que ces dossiers existent
-if not os.path.exists('templates'):
-    os.makedirs('templates')
-if not os.path.exists('static'):
-    os.makedirs('static')
+# Assurez-vous que ces dossiers existent 
+if not os.path.exists('templates'):     
+    os.makedirs('templates') 
+if not os.path.exists('static'):     
+    os.makedirs('static')  
 
-# Configurez les routes
-configure_routes(app)
+# Ajout de la fonction pour désactiver le cache
+@app.after_request
+def add_header(response):
+    if 'Cache-Control' not in response.headers:
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '0'
+    return response
 
-if __name__ == '__main__':
-    app.logger.debug("Démarrage de l'application")  # Ajout d'un log de démarrage
+# Configurez les routes 
+configure_routes(app)  
+
+if __name__ == '__main__':     
+    app.logger.debug("Démarrage de l'application")  # Ajout d'un log de démarrage     
     app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
-    
